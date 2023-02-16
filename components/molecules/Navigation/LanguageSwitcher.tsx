af4fbf98ca44c23c75b06de085d1b0e12600aee9
Lee <lee20010905@gmail.com>
2023-02-17 00:01:55 +0900

    Fix LanguageSwitcher and finished

--- components/molecules/Navigation/LanguageSwitcher.tsx
+++ components/molecules/Navigation/LanguageSwitcher.tsx
@@ -1,27 +1,67 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
+// MUI
+import { Button, Menu, MenuItem } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpSharpIcon from '@mui/icons-material/KeyboardArrowUpSharp';
+import PublicSharpIcon from '@mui/icons-material/PublicSharp';
 
 export default function LanguageSwitcher() {
   const router = useRouter();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const languageOpen = Boolean(anchorEl)
+  
+  const handleClick = useCallback((
+    event: React.MouseEvent<HTMLElement>
+    ) => {
+      setAnchorEl(event.currentTarget);
+    }, []
+  );
+  const handleClose = useCallback((
+    ) => {
+      setAnchorEl(null);
+    }, []
+  );
 
   const { locales, locale: activeLocale } = router;
-
+  // Get languages not currently selected
   const otherLocales = locales?.filter(
-    (locale) => locale !== activeLocale && locale !== "ja"
+    (locale) => locale !== activeLocale
   );
 
   return (
-    <span>
+    <div className="language_switcher_button">
+      <Button
+        id="language_button"
+        aria-controls={languageOpen ? 'basic-menu' : undefined}
+        variant="contained"
+        aria-expanded={languageOpen ? 'true' : undefined}
+        onClick={handleClick}
+        startIcon={<PublicSharpIcon/>}
+        endIcon={languageOpen ? <KeyboardArrowUpSharpIcon/> :<KeyboardArrowDownIcon/>}
+      >
+        {activeLocale === "ja" ? "japan" : activeLocale === "en" ? "English" : "Language"}
+      </Button>
+      <Menu
+        open={languageOpen}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
         {otherLocales?.map((locale) => {
           const { pathname, query, asPath } = router;
           return (
-            <span key={"locale-" + locale}>
+            <MenuItem key={"locale-" + locale}>
               <Link href={{ pathname, query }} as={asPath} locale={locale}>
-                {locale === "en" ? "English" : locale === "ja" ? "japanese" : null}
+                {locale === "ja" ? "japan" : locale === "en" ? "English": "unable"}
               </Link>
-            </span>
+            </MenuItem>
           );
         })}
-    </span>
+      </Menu>
+    </div>
   );
 }
