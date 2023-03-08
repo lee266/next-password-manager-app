bb1e2983fcb1416eea2054d724865e94ba3c6b46
Lee <lee20010905@gmail.com>
2023-03-08 16:10:59 +0900

    Fix setting files

--- next.config.js
+++ next.config.js
@@ -1,21 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  *  @type {import('next').NextConfig} 
 */
 
 const { i18n } = require('./next-i18next.config');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  webpack:(config, options) => {
+    require('./webpack.config')(config, options);
+    return config;
+  },
   // hot reload 
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 5000,
-      aggregateTimeout: 300,
-      ignored : ['node_modules']
-    }
-    return config
-  }, 
+  // webpackDevMiddleware: config => {
+  //   config.watchOptions = {
+  //     poll: 5000,
+  //     aggregateTimeout: 300,
+  //     ignored : ['node_modules']
+  //   }
+  //   return config
+  // }, 
 };
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
+// module.exports = nextConfig;
