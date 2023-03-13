1f02a4f89e37358096ae2a4d50d8fd24933ca3ad
Lee <lee20010905@gmail.com>
2023-03-13 16:39:33 +0900

    Fix login

---
+++ src/components/molecules/Form/LoginForm.tsx
@@ -0,0 +1,127 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+// translation
+import { useTranslation } from "next-i18next";
+// validation rule
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useDispatch } from "react-redux";
+import { addAlert } from "redux/Feedback/reducer";
+import { Alert } from "redux/Feedback/types";
+import { fetchUserDataRequest } from "redux/users/reducer";
+// MUI
+import Button from '@mui/material/Button';
+import CircularProgress from "@mui/material/CircularProgress";
+import TextField from '@mui/material/TextField';
+import { createJwt } from "api/auth/jwt";
+import { saveToken } from "utils/auth";
+
+type LoginFromData = {
+  email: string
+  password: string
+}
+
+const LoginForm = () => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [alertMessage, setAlertMessage] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  // Define yup Error Messages
+  const requiredError = t("general.yup.required");
+  const requiredEmail = t("general.yup.email");
+  // validation rule 
+  const schema = yup.object().shape({
+    email: yup.string()
+      .trim()
+      .email(requiredEmail)
+      .required(requiredError),
+    password: yup.string()
+      .required(requiredError)
+  });
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm<LoginFromData>({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit: SubmitHandler<LoginFromData> = async(data) => {
+    try {
+      setIsSubmitting(true);
+      const token = await createJwt(data);
+      const accessKey:string = token.data.access;
+      // Save token in cookie 
+      saveToken(accessKey);
+      dispatch(fetchUserDataRequest({token: accessKey}));
+      setAlertMessage("create jwt");
+      const alert: Alert = {
+        message: alertMessage,
+        severity: "success",
+      }
+      dispatch(addAlert(alert));
+      await router.push("/");
+    } catch (error) {
+      if (error instanceof Error) {
+        setAlertMessage(error.message)
+      }else{
+        setAlertMessage("error")
+      }
+      const alert: Alert = {
+        message: alertMessage,
+        severity: "error",
+      }
+      dispatch(addAlert(alert))
+    }finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  return(
+    <div className="login-form">
+      <form>
+        <TextField
+          id="email"
+          label={t("general.auth.email")}
+          margin="normal"
+          required
+          fullWidth
+          autoComplete="email"
+          autoFocus
+          {...register('email')}
+          error={!!errors.email}
+          helperText={errors.email?.message}
+        />
+        <TextField
+          id="password"
+          label={t("general.auth.password")}
+          margin="normal"
+          required
+          fullWidth
+          type="password"
+          autoComplete="current-password"
+          {...register('password')}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          onClick={handleSubmit(onSubmit)}
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? 
+            <CircularProgress size={24} color="primary" /> : t("component.button.login")
+          }
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm;
--- src/pages/login2.tsx
+++ src/pages/login2.tsx
@@ -1,182 +1,33 @@
-import { useState } from "react";
-/* eslint-disable react-hooks/rules-of-hooks */
 import { GetStaticProps, NextPage } from "next";
-import styles from '../styles/Home.module.scss';
 // translation
-import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-// validation rule
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import { createJwt } from "api/auth/jwt";
-// Components 
-import Alerts from "components/molecules/Feedback/Alerts";
-// Redux
-import { useRouter } from "next/router";
-import { useDispatch } from "react-redux";
-import { fetchUserDataRequest } from "redux/users/reducer";
+import styles from '../styles/Home.module.scss';
 // MUI 
 import Box from "@mui/material/Box";
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Copyright from '../components/molecules/Copyright';
-import { AlertColor } from "@mui/material";
-import { saveToken } from "utils/auth";
-import { useSelector } from "react-redux";
-import { RootState } from "redux/rootReducer";
 import Alerts2 from "components/molecules/Feedback/Alerts2";
+import Login from "components/organisms/Auth/Login/Login";
 
-
-type loginType = {
-  locale: string
-}
-
-type AddFromType = {
-  email: string
-  user: number
-  password:string
-}
-
-
-const login2: NextPage<loginType> = ( props ) => {
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const user = useSelector((state: RootState) => state.user.user);
-  const router = useRouter();
-  // Alert States
-  const [alertOpen, setAlertOpen] = useState<boolean>(false);
-  const [severity, setSeverity] = useState<AlertColor>("info");
-  const [alertMessage, setAlertMessage] = useState<string>("");
-  // validation rule 
-  const schema = yup.object().shape({
-    email: yup.string()
-      .required(t("general.yup.required")!)
-      .email("Invalid email address"),
-    password: yup.string()
-      .required(t("general.yup.required")!),
-  });
-
-  const { 
-    register, 
-    handleSubmit, 
-    formState: { errors } 
-  } = useForm<AddFromType>({
-    resolver: yupResolver(schema)
-  });
-
-  const onSubmit: SubmitHandler<AddFromType> = async(data) => {
-    const token = await createJwt(data);
-    if (token) {
-      console.log(token);
-      saveToken(token.data.access);
-      const accessKey:string = token.data.access
-      dispatch(fetchUserDataRequest({token: accessKey}))
-      console.log("user",user);
-      // console.log("delete user", user);
-      router.push("/");
-    }else{
-      setAlertOpen(true)
-      setSeverity("error")
-      setAlertMessage('loginに失敗しました')
-    }
-  }
-
+const loginPage: NextPage = () => {
   return (
-    <Box className={styles.main}>
-      <Container component="main" maxWidth="xs">
-        <Alerts
-          alertOpen={alertOpen}
-          alertMessage={alertMessage}
-          setAlertOpen={setAlertOpen}
-          severity={severity}
-        />
+    <div className="login-section">
+      <Box className={styles.main}>
         <Alerts2/>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            {t("general.auth.login")}
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
-            <TextField
-              id="email"
-              label={t("general.auth.email")}
-              margin="normal"
-              required
-              fullWidth
-              autoComplete="email"
-              autoFocus
-              {...register('email')}
-              error={!!errors.email}
-              helperText={errors.email?.message}
-            />
-            <TextField
-              id="password"
-              label={t("general.auth.password")}
-              {...register('password')}
-              margin="normal"
-              required
-              fullWidth
-              type="password"
-              autoComplete="current-password"
-              error={!!errors.password}
-              helperText={errors.password?.message}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              {t("component.button.login")}
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  {t("general.auth.forgotPassword") + "?"}
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/sign-up" variant="body2">
-                  {t("general.auth.noAccount") + "?" + t("general.auth.pleaseSignUp")}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </Box>
+        <Container component="main" maxWidth="xs">
+          <Login/>
+        </Container>
+      </Box>
+    </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async({
-  locale,
-}) => {
-  // please check log in docker password_next  if you want to see console.log
-  console.log(locale);
-  const translations = await serverSideTranslations(locale!, ["common"])
+export const getStaticProps: GetStaticProps = async ({locale}) => {
+  if (!locale) { locale = 'ja' }
   return {
     props: {
-      locale,
-      ...translations
-    },
+      ...(await serverSideTranslations(locale, ["common"])),
+    }
   }
 }
 
-export default login2;
+export default loginPage;
