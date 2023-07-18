1809046f8816888206fec2239f9430614f3b0aa2
Lee <lee20010905@gmail.com>
2023-07-19 03:30:18 +0900

    fix error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,8 @@
  * https://github.com/i18next/next-i18next
  */
 
-import path from 'path';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 module.exports = {
   i18n: {
@@ -11,6 +12,6 @@ module.exports = {
     locales: ["ja", "en"],
     // localeDetection: false,
     // please set path of  locales folder
-    localePath: path.resolve('./public/locales'),
-  }
+  },
+  localePath: path.resolve('./public/locales'),
 }
