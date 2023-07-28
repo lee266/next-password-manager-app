4ec406f1ca5b494be1bbbeea487005f59c87a277
Lee <lee20010905@gmail.com>
2023-07-28 13:00:52 +0900

    Add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -10,10 +11,17 @@ module.exports = {
       colors: {
         primary: {
           DEFAULT: '#1976D2',
-        } ,
+          dark: '#123490',
+        },
         accent: {
           DEFAULT: '#24AAB4',
-        }
+          dark: '#789abc',
+        },
+        back: {
+          DEFAULT: '#ffffff',
+          dark: '#091A47',
+          rightDark: '#1A2229'
+        },
       }
     },
   },
