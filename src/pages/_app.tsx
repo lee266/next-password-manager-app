6d0e1abe33c3a301e18b66f7e6a980661727e268
Lee <lee20010905@gmail.com>
2023-07-28 23:27:25 +0900

    Fix darkMode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,6 @@ import createEmotionCache from '../createEmotionCache';
 import { appWithTranslation } from 'next-i18next';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
-import Script from 'next/script';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -24,7 +23,6 @@ const MyApp = (props: MyAppProps) => {
 
   return (
     <Provider store={store}>
-      <Script src="/js/tailwindDarkMode.js" strategy="afterInteractive" />
       <CacheProvider value={emotionCache}>
         <Head>
           <title>Password manager</title>
